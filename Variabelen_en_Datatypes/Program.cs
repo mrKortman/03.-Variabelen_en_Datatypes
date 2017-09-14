b5e03b52ccbddbbea974637d37f5f1ea14aed894
Boy Kortman <b.kortman@leeuwenborgh.nl>
2017-09-14 10:26:48 +0200

    Handout toegevoegd in de map Docs, deze is gebruikt in de les omm de stof te behandelen.

--- Variabelen_en_Datatypes/Program.cs
+++ Variabelen_en_Datatypes/Program.cs
@@ -10,6 +10,21 @@ namespace Variabelen_en_Datatypes
     {
         static void Main(string[] args)
         {
+
+            //Dit programma vraagt om een getal en converteerd deze dan naar een ander type.
+
+            Console.Clear();
+
+            Console.WriteLine("Geef een getal op :");
+            string strGetal = Console.ReadLine();
+            Console.WriteLine("U heeft het getal " + strGetal + " opgegeven dat is van het type : " + strGetal.GetType().ToString());
+            Console.WriteLine("Converting...");
+            int intGetal = Convert.ToInt32(strGetal);
+            Console.WriteLine("Done!");
+            Console.WriteLine("Het getal is nu gevonverteerd naar het type : " + intGetal.GetType().ToString());
+            Console.ReadLine();
+
+
         }
     }
 }
